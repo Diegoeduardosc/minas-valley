c1a06ff08b44bf64d4789e543a10fe9c70264455
Diegoeduardosc <72477468+Diegoeduardosc@users.noreply.github.com>
2020-11-23 22:51:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<title>MINAS VALLEY - v1.1</title>
+<title>MINAS VALLEY</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -96,7 +96,7 @@ body, html {
     <div class="w3-row-padding" id="about">
         <div class="w3-col l4 12"><br><br><br>
           <h1> -- -- -- -- T.E.S.T.E -- -- -- -- </h1>
-          <h1>Descubra as mais promissoaras Startups do Brasil e conecte-se a elas</h1><br>
+          <h1>Descubra as mais promissoaras Startups do Brasil e conecte-se a elas.</h1><br>
           <h4>A plataforma que veio para unir Startups, Colaboradores e Investidores tudo em um só lugar. Mergulhe neste novo mar de experiência conosco.</h4>
           <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h4>
         </div>
